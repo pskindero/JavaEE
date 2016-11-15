fb1fb5a1e1e82f4ad92333c7ca370c822f585a2e
pskindero <skinderowicz.patryk@gmail.com>
2016-11-15 20:09:29 +0100

    [bean-validation] Implementation of custom annnotation for bean validation

---
+++ bean_validation/custom_annotation/src/main/java/com/pskindero/javaee/bean_validation/custom_annotation/App.java
@@ -0,0 +1,13 @@
+package com.pskindero.javaee.bean_validation.custom_annotation;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ bean_validation/custom_annotation/src/main/java/com/pskindero/javaee/bean_validation/custom_annotation/SomeBean.java
@@ -0,0 +1,5 @@
+package com.pskindero.javaee.bean_validation.custom_annotation;
+
+public class SomeBean {
+
+}
